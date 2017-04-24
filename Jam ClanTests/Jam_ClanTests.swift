5875d1e4b7063efdea988e231e9e7cc66372cf58
Saurabh singh rathore <saurabh@wemo.in>
2017-04-24 10:20:54 +0530

    Initial Commit

---
+++ Jam ClanTests/Jam_ClanTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jam_ClanTests.swift
+//  Jam ClanTests
+//
+//  Created by Saurabh singh rathore on 24/04/17.
+//  Copyright © 2017 Saurabh singh rathore. All rights reserved.
+//
+
+import XCTest
+@testable import Jam_Clan
+
+class Jam_ClanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
